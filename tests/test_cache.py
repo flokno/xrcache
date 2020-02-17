4f86fd0e4676b18178a5fbf3ff6d8ea1698ce18c
Florian Knoop <florian_knoop@gmx.de>
2020-02-17 10:46:34 +0100

    v0.0.3 | add array/dataset name to filename in the storage
    * introduce `@stored`
    * simplify attached data
    * add optional verbosity
    * update test and doc

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -2,15 +2,18 @@ import xarray as xr
 
 import xrcache as xc
 
-# create Dataset
-ds = xr.Dataset({"bar": ("x", [1, 2, 3, 4]), "x": list("abcd")})
+dataset_name = "dataset"
+dataarray_name = "bar"
 
-hash_str = xc.get_hash(ds.bar.data)
+# create DataArray and Dataset
+ds = xr.Dataset({dataarray_name: ("x", [1, 2, 3, 4]), "x": list("abcd")})
+ds.attrs.update({xc.keys.name: dataset_name})
+ds.attrs.update({xc.keys.hash: xc.get_hash(ds.bar.data)})
 
-ds.attrs.update({xc.keys.hash: hash_str})
+da = ds.bar
+da.attrs.update({xc.keys.hash: xc.get_hash(da.data)})
 
 
-# define function that uses cache
 @xc.cached
 def some_dataset_function(dataset: xr.Dataset) -> xr.Dataset:
     """add square of input"""
@@ -21,16 +24,36 @@ def some_dataset_function(dataset: xr.Dataset) -> xr.Dataset:
     return new_ds
 
 
+@xc.cached(verbose=True)
+def some_dataarray_function(array: xr.DataArray) -> xr.DataArray:
+    """add square of input"""
+    return array ** 2
+
+
 def test_cache_dataset(tmp_path):
     """run test in tmp folder"""
     xc._path_cache = tmp_path
 
     a = some_dataset_function(ds)
 
-    assert (xc.path_cache() / "some_dataset_function.nc").exists()
+    assert (xc.path_cache() / f"{dataset_name}__some_dataset_function.nc").exists()
     assert xc.path_log().exists()
 
     b = some_dataset_function(ds)
+
+    assert a.equals(b)
+    assert a.attrs == b.attrs
+
+
+def test_cache_dataarray(tmp_path):
+    xc._path_cache = tmp_path
+
+    a = some_dataarray_function(da)
+
+    assert (xc.path_cache() / f"{dataarray_name}__some_dataarray_function.nc").exists()
+    assert xc.path_log().exists()
+
+    b = some_dataarray_function(da)
 
     assert a.equals(b)
     assert a.attrs == b.attrs
